697f651aaf4914cc15f0e985ea3bbdf985262762
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-02-27 17:02:34 +0900

    編集機能の実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import { Key, useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 type Props = {
-  items: Array<{id: Key, title: String}>;
+  items: Array<{id: string, title: string}>;
 };
 
 export default function Home({ items }: Props) {
   const [value, setValue] = useState('')
-  const deleteItem = (id: Key) => {
+  const router = useRouter()
+  const deleteItem = (id: string) => {
     async function deleteTask() {
       const response = await fetch(`https://gwp3bt9vq3.execute-api.ap-northeast-1.amazonaws.com/deleteTaskById?itemId=${id}`, {method: 'DELETE'})
       const data = response.json();
@@ -41,7 +44,7 @@ export default function Home({ items }: Props) {
         <input type="text" onChange={(e) => setValue(e.target.value)} />
         <button onClick={() => addTask(value)}>追加</button>
         <h3>タスク一覧</h3>
-        {items.map((item) => {return <div key={item.id}>{item.title} <button onClick={() => deleteItem(item.id)}>削除</button></div>})}
+        {items.map((item) => {return <div key={item.id}><Link href={`/tasks/${item.id}`}>{item.title}</Link><button onClick={() => deleteItem(item.id)}>削除</button><button onClick={()=>router.push(`/tasks/${item.id}`)}>編集</button></div>})}
       </main>
     </>
   )
